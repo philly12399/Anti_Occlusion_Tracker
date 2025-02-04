dc7ce5e1d450f121bf1dc8e901831074450d1aa5
philly <philly12399@gmail.com>
2025-02-05 01:48:30 +0800

    demo config

--- eval.sh
+++ eval.sh
@@ -1,3 +1,4 @@
 cd ./scripts/KITTI
-python3 evaluate.py --config ./configs/KITTI_det_car.yml
+python3 evaluate.py --config ./configs/KITTI_demo.yml
+# python3 evaluate.py --config ./configs/KITTI_demo_post.yml
 cd ../../
